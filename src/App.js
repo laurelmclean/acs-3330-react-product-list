b2a6eb395370a8474b94991a4e9881b73c340f21
Laurel McLean <laurel.mclean@live.ca>
2024-01-16 15:00:09 -0700

    challenge 3 completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './data'
 
 function App() {
   return (
--- src/data.js
+++ src/data.js
@@ -2,6 +2,11 @@ import data from './data.json';
 
 const allCategories = data.map(obj => obj.category);
 
-console.log(allCategories);
+// Make a set from an array 
+// all values of the set are unique
+const categorieSet = new Set(allCategories)
+const categoriesUnique = Array.from(categorieSet)
+
+console.log(categoriesUnique);
 
 export default data;
