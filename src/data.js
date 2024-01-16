dbedcb7653d3ac78bf36ff6b28fe56211a7ca0f8
Laurel McLean <laurel.mclean@live.ca>
2024-01-16 15:18:07 -0700

    challenge 4 complete

--- src/data.js
+++ src/data.js
@@ -7,6 +7,22 @@ const allCategories = data.map(obj => obj.category);
 const categorieSet = new Set(allCategories)
 const categoriesUnique = Array.from(categorieSet)
 
-console.log(categoriesUnique);
+// histogram where keys are names of the categories
+// and values are number of times it appears
+const categoriesWithCounts = data.reduce((obj, item) => {
+    const category = item.category;
+
+    // Check if the category key exists in the object
+    // If it doesn't, initialize it with a count of 1
+    // If it does, increment the count
+    if (!obj[category]) {
+        obj[category] = 1;
+    } else {
+        obj[category] += 1;
+    }
+    return obj;
+}, {});
+
+console.log(categoriesWithCounts);
 
 export default data;
